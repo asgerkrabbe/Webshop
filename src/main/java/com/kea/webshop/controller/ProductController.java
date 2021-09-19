64d6d1770d8d40661589d0c977bf95e2f1f85ec4
asgerkrabbe <asgerkrabbe@gmail.com>
2021-09-19 20:13:17 +0200

    updates to controller

--- src/main/java/com/kea/webshop/controller/ProductController.java
+++ src/main/java/com/kea/webshop/controller/ProductController.java
@@ -1,14 +1,20 @@
 package com.kea.webshop.controller;
 
+import com.kea.webshop.model.Product;
 import com.kea.webshop.repository.ProductRepository;
 import com.kea.webshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class ProductController {
+
+    Product product;
 
     @Autowired
     ProductService productService;
@@ -20,4 +26,32 @@ public class ProductController {
 
         return "index.html";
     }
+
+    @GetMapping("/product")
+    public String inspectProduct(@RequestParam long id) {
+        productService.findById(id);
+
+        return "product.html";
+    }
+
+    @PostMapping("/update-product")
+    public String updateProduct(@RequestParam long id, WebRequest request) {
+        String name = request.getParameter("name");
+        String price = request.getParameter("price");
+
+        Product product = new Product(id, name, Integer.parseInt(price));
+        productService.editProduct(product);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete")
+    public String delete(@RequestParam long id) {
+        productService.delete(id);
+
+    return "redirect:/";
+    }
+
+
+
+
 }
--- src/main/java/com/kea/webshop/model/Product.java
+++ src/main/java/com/kea/webshop/model/Product.java
@@ -19,6 +19,12 @@ public class Product {
         this.price = price;
     }
 
+    public Product(Long id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
     public Product() {
 
     }
